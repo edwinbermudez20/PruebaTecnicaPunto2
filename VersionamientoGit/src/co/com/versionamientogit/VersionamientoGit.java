d546ab5abbb72ea238868e3b747c4402866d2c81
edwinbermudez20 <70782633+edwinbermudez20@users.noreply.github.com>
2020-09-06 03:55:31 -0500

    Se crea cambio para rama Hotfix

--- VersionamientoGit/src/co/com/versionamientogit/VersionamientoGit.java
+++ VersionamientoGit/src/co/com/versionamientogit/VersionamientoGit.java
@@ -4,6 +4,7 @@ public class VersionamientoGit {
 
 	public static void main(String[] args) {
 		System.out.println("Se versiona proyecto inicial");
+		System.out.println("Se crea cambio para rama Hotfix");
 	}
 
 }
