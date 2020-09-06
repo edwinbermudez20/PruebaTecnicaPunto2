74ecd2c31cfa409296774b25447e87cd4b3e609b
edwinbermudez20 <70782633+edwinbermudez20@users.noreply.github.com>
2020-09-06 04:16:25 -0500

    Se crea cambio para rama Feature2 cambio1

--- VersionamientoGit/src/co/com/versionamientogit/VersionamientoGit.java
+++ VersionamientoGit/src/co/com/versionamientogit/VersionamientoGit.java
@@ -5,6 +5,9 @@ public class VersionamientoGit {
 	public static void main(String[] args) {
 		System.out.println("Se versiona proyecto inicial");
 		System.out.println("Se crea cambio para rama Hotfix");
+		
+		
+		System.out.println("Se crea cambio para rama Feature2 cambio 1");
 	}
 
 }
